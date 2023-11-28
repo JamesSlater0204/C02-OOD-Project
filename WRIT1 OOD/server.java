b0c5c63893b006172c43de361f44487e191df62d
JamesSlater0204 <130367333+JamesSlater0204@users.noreply.github.com>
2023-11-28 17:47:20 +0000

    Update server.java

--- WRIT1 OOD/server.java
+++ WRIT1 OOD/server.java
@@ -4,7 +4,7 @@ import java.util.concurrent.*;
 
 
 public class server {
-
+    //james editing 
     private static final int PORT = 444; // Change this to your desired port number
     private static final int MAX_CLIENTS = 4;
     private static ExecutorService executor = Executors.newFixedThreadPool(MAX_CLIENTS);
